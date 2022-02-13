54af543e89c7d71a3b5e45cddccaac4a5e49000a
Kyohei-us <kyohei1212000@gmail.com>
2022-02-13 02:21:24 -0800

    Add: limit, offset, fields. Standardize naming.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Post, PrismaClient } from "@prisma/client";
 import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
@@ -16,94 +16,89 @@ dotenv.config();
 
 // routes here
 
+// Users
+
 /*
 Show all users
 */
 app.get("/users", async (req, res) => {
+
+  // default: 10
+  let skip = 0;
+  if (req.query.offset) {
+    const offset = req.query.offset as string;
+    skip = parseInt(offset)
+  }
+  let take = 10;
+  if (req.query.limit) {
+    const limit = req.query.limit as string;
+    take = parseInt(limit);
+  }
+
+  console.log(skip, take)
+
   const allUsers = await prisma.user.findMany({
+    skip: skip,
+    take: take,
     include: {
-      posts: true,
-      profile: true,
+      posts: false,
+      profile: false,
     },
   });
   console.dir(allUsers, { depth: null });
-  res.json(allUsers);
+  res.json({users: allUsers, length: allUsers.length});
 });
 
+/**
+ * Get a user by id
+ */
 app.get("/user/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  const user = await prisma.user.findUnique({
-    where: {
-      id: id
-    },
-    include: {
-      profile: true,
-      posts: true
+  try {
+    const fieldsString = req.query.fields as string;
+    let fields: string[] = [];
+    if (fieldsString) {
+      fields = fieldsString.split(',');
     }
-  });
-  console.log(user);
-  res.json(user);
-})
 
-/*
-Show all posts
-if authorId is present in req.body params,
-show posts by the author
-*/
-app.get("/posts", async (req, res) => {
-  if (req.body.authorId){
-    const allPosts = await prisma.post.findMany({
+    const id = parseInt(req.params.id);
+
+    const verified = protectedRoute(req);
+    if (!verified) {
+      return res.status(401).send({message: "Access Denied"});
+    }
+
+    const withProfile = fields.includes("profile") ? true : false;
+    const withPosts = fields.includes("posts") ? true : false;
+
+    const user = await prisma.user.findUnique({
       where: {
-        authorId: req.body.authorId as number
+        id: id
+      },
+      include: {
+        profile: withProfile,
+        posts: withPosts
       }
     });
-    console.dir(allPosts, { depth: null });
-    res.json(allPosts);
-  }else{
-    const allPosts = await prisma.post.findMany();
-    console.dir(allPosts, { depth: null });
-    res.json(allPosts);
+
+    console.log(user);
+    res.json(user);
+  } catch (error) {
+    // Access Denied
+    return res.status(401).send(error);
   }
-});
+
+})
 
 /*
 Add user
 Required body params: email, name
 */
-app.post("/addUser", async (req, res) => {
+app.post("/user", async (req, res) => {
   try {
     // add post method here
     let newUser = await addUser(req);
     return res.send(newUser);
     // return res.send("Successfully Verified");
-  } catch (error) {
-    // Access Denied
-    return res.status(401).send(error);
-  }
-})
-
-/*
-Add user
-Required body params: title, content, authorId, user token (key is "token header key")
-*/
-app.post("/addPost", async (req, res) => {
-  try {
-    const verified = protectedRoute(req);
-
-    // Access Denied
-    if (!verified) return res.status(401).send({ message: "Access denied" });
-    else {
-      // add post method here
-      console.log("Adding post...");
-      let newPost = await addPost(req);
-      if (newPost) {
-        console.log("Post added");
-        return res.send({ title: newPost.title });
-      } else {
-        console.log("Post is not added");
-        return res.send({ message: "Post is not added" })
-      }
-    }
   } catch (error) {
     // Access Denied
     return res.status(401).send(error);
@@ -152,6 +147,80 @@ app.get("/user/validateUserToken", (req: express.Request, res: express.Response)
   }
 
 })
+
+// End of Users
+
+// Posts
+
+/*
+Show all posts
+if authorId is present in req.body params,
+show posts by the author
+*/
+app.get("/posts", async (req, res) => {
+
+  // default: 10
+  let skip = 0;
+  if (req.query.offset) {
+    const offset = req.query.offset as string;
+    skip = parseInt(offset)
+  }
+  let take = 10;
+  if (req.query.limit) {
+    const limit = req.query.limit as string;
+    take = parseInt(limit);
+  }
+
+if (req.body.authorId){
+  const allPosts = await prisma.post.findMany({
+    skip: skip,
+    take: take,
+    where: {
+      authorId: req.body.authorId as number
+    }
+  });
+  console.dir(allPosts, { depth: null });
+  res.json({posts: allPosts, length: allPosts.length});
+}else{
+  const allPosts = await prisma.post.findMany({
+    skip: skip,
+    take: take
+  });
+  console.dir(allPosts, { depth: null });
+  res.json({posts: allPosts, length: allPosts.length});
+}
+});
+
+
+/*
+Add user
+Required body params: title, content, authorId, user token (key is "token header key")
+*/
+app.post("/post", async (req, res) => {
+  try {
+    const verified = protectedRoute(req);
+
+    // Access Denied
+    if (!verified) return res.status(401).send({ message: "Access denied" });
+    else {
+      // add post method here
+      console.log("Adding post...");
+      let newPost = await addPost(req);
+      if (newPost) {
+        console.log("Post added");
+        return res.send({ title: newPost.title });
+      } else {
+        console.log("Post is not added");
+        return res.send({ message: "Post is not added" })
+      }
+    }
+  } catch (error) {
+    // Access Denied
+    return res.status(401).send(error);
+  }
+})
+
+// End of Posts
 
 app.get("/", async (req, res) => {
   res.json({ message: "Home Page" })
